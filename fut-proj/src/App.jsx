12f13defecfedbf2da8aacd1614f5ec2f6d49ac8
Sintu Kumar <shiva49767@gmail.com>
2026-08-08 13:07:14 +0530

    create a theme state provide to navbar

--- fut-proj/src/App.jsx
+++ fut-proj/src/App.jsx
@@ -3,15 +3,16 @@ import { Outlet } from 'react-router-dom'
 
 import './App.css'
 import Navbar from './components/Navbar'
+import { useState } from 'react'
 
 function App() {
-
+  const [theme, setTheme] = useState('light')
 
   return (
-    <>
-      <Navbar />
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme} />
       <Outlet />
-    </>
+    </div>
   )
 }
 
