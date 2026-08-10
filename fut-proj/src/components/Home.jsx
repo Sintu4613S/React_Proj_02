a249b1203faa120eb475654380964ca1b3b09244
Sintu Kumar <shiva49767@gmail.com>
2026-08-10 18:10:12 +0530

    feat: update Home component to include Trusetd and Service components

--- fut-proj/src/components/Home.jsx
+++ fut-proj/src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Hero from './pages/Hero'
 
+import Trusetd from './pages/Trusetd'
+import Service from './pages/Service'
+
 const Home = () => {
   return (
     <>
       <Hero />
+      <Trusetd />
+      <Service />
     </>
   )
 }
