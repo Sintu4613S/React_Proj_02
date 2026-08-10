aefd0c2f41ee976bac22c45a010feee2f0fd97c8
Sintu Kumar <shiva49767@gmail.com>
2026-08-10 18:21:21 +0530

    Create a OurWork page and add the title and description throgh title page

---
+++ fut-proj/src/components/pages/OurWork.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from './Title'
+
+const OurWork = () => {
+  return (
+    <div className='flex items-center gap-5 flex-col px-4 sm:px-12 lg:px-24 xl:px-40 pt-20 text-gray-700 dark:text-white'>
+      <Title title='Our Latest Work' desc='From Strategy to execution, We credit digital solutions that move your business forward.' />
+
+    </div>
+  )
+}
+
+export default OurWork
