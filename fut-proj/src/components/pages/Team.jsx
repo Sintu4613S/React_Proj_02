5e6bc1b5dca3e3db2ed43a1d0d0cfb631d8bf384
Sintu Kumar <shiva49767@gmail.com>
2026-08-11 11:24:32 +0530

    Enhance Team component styling with hover effects and transitions

--- fut-proj/src/components/pages/Team.jsx
+++ fut-proj/src/components/pages/Team.jsx
@@ -11,7 +11,7 @@ const Team = () => {
       <div className='  w-full max-w-5xl grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5'>
 
         {teamData.map((info, index) => (
-          <div key={index} className='flex gap-5 py-5 items-center justify-center'>
+          <div key={index} className='flex gap-5 py-5 items-center justify-center hover:scale-105 duration-500 transition-all cursor-pointer  bg-gray-100 rounded-xl'>
             <img src={info.image} className='w-12 h-12 rounded-full  ' alt="img" />
             <div>
               <h3 className='font-bold text-xl '>{info.name}</h3>
