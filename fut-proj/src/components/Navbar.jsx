e1de97d9eb782d17c230dd6cbee984cc503b71c6
Sintu Kumar <shiva49767@gmail.com>
2026-08-11 16:58:17 +0530

    refactor the route patha and comment the theme

--- fut-proj/src/components/Navbar.jsx
+++ fut-proj/src/components/Navbar.jsx
@@ -16,14 +16,14 @@ const Navbar = ({ theme, setTheme }) => {
         <div className={`flex gap-4 text-gray-700 dark:text-white sm:text-sm ${!sidebaropen ? 'max-sm:w-0 overflow-hidden' : 'max-sm:w-60 max-sm:pl-10'} max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20  sm:items-center transition-all`}>
           <img src={assets.close_icon} className='w-5 absolute right-4 top-4 sm:hidden cursor-pointer' onClick={() => setSidebaropen(false)} alt="" />
           <Menu to={'/'} title={'Home'} />
-          <Menu to={'/services'} title={'Services'} />
+          <Menu to={'/service'} title={'Service'} />
           <Menu to={'/our-work'} title={'Our Work'} />
           <Menu to={'/contact-us'} title={'Contact Us'} />
 
         </div>
 
         <div className='flex items-center gap-2 sm:gap-4' >
-          <ThemeButton theme={theme} setTheme={setTheme} />
+          {/* <ThemeButton theme={theme} setTheme={setTheme} /> */}
           <img src={theme === 'dark' ? assets.menu_icon_dark : assets.menu_icon} alt="" onClick={() => setSidebaropen(true)} className='w-8 sm:hidden' />
 
           <NavLink className='text-sm max-sm:hidden  bg-primary text-white px-6 py-2 rounded-full cursor-pointer hover:scale-105 transition-all flex items-center gap-2' to={'/contact-us'} >Connect
