5f28e917860e74655ce5dd51168e3be8a73880b8
Sintu Kumar <shiva49767@gmail.com>
2026-08-11 11:24:59 +0530

    Add Contact and WorkPage components with basic structure

--- fut-proj/src/components/Home.jsx
+++ fut-proj/src/components/Home.jsx
@@ -4,14 +4,20 @@ import Hero from './pages/Hero'
 import Trusetd from './pages/Trusetd'
 import Service from './pages/Service'
 import OurWork from './pages/OurWork'
+import Team from './pages/Team'
+import ContactForm from './pages/contactForm'
+import { Toaster } from 'react-hot-toast'
 
 const Home = () => {
   return (
     <>
+      <Toaster />
       <Hero />
       <Trusetd />
       <Service />
       <OurWork />
+      <Team />
+      <ContactForm />
     </>
   )
 }
