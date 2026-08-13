6b376b3349c6fed03721c1eaeaed904b89297d9b
Sintu Kumar <shiva49767@gmail.com>
2026-08-13 15:41:06 +0530

    add the card anf bottom  section of page

--- gemini-proj/src/components/main/Mainbox.jsx
+++ gemini-proj/src/components/main/Mainbox.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Mainbox.css'
+import Card from './cards/Card'
+import { assets } from '../../assests/assest'
 
 const Mainbox = () => {
   return (
@@ -8,7 +10,20 @@ const Mainbox = () => {
         <p><span>Hello, Dev.</span></p>
         <p>How can I help you today ?</p>
       </div>
+      <Card />
+      {/* Bottom Part or Search part */}
+      <div className='bottom-container'>
+        <div className='search-box'>
+          <input type="text" placeholder='Enter prompt here' />
+          <div className='flex gap-1'>
+            <img src={assets.gallery_icon} className='w-5' alt="" />
+            <img src={assets.gallery_icon2} className='w-5' alt="" />
+          </div>
+        </div>
+        <p className='text-center text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident facere accusantium ipsam nisi ullam odio nihil fugiat quas. Reiciendis, vero!</p>
 
+
+      </div>
     </div>
   )
 }
