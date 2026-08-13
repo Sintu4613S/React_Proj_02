2d2ce8e26f616c10ac7392fb024d7f251593b32e
Sintu Kumar <shiva49767@gmail.com>
2026-08-13 15:40:12 +0530

    create a card component

---
+++ gemini-proj/src/components/main/cards/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Card.css'
+import { assets } from '../../../assests/assest'
+
+const Card = () => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5'>
+
+      <Singlecard title='heloo' imageSrc={assets.bulb_icon} />
+      <Singlecard title='heloo' imageSrc={assets.code_icon} />
+      <Singlecard title='heloo' imageSrc={assets.compass_icon} />
+      <Singlecard title='heloo' imageSrc={assets.message_icon} />
+
+    </div>
+  )
+}
+
+export default Card
+const Singlecard = ({ title, imageSrc }) => {
+  return (
+    <div className='card relative'>
+      <p>{title}</p>
+      <img src={imageSrc} className='w-8 absolute bottom-3 right-3 rounded-full bg-white' alt="" />
+    </div>
+  )
+
+}
