8bf5a52319c27de6e080eac53f84b0eb7cd90279
Sintu Kumar <shiva49767@gmail.com>
2026-08-13 13:31:04 +0530

    create a navbar component

---
+++ gemini-proj/src/components/main/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { assets } from '../../assests/assest'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-15 flex justify-between items-center' style={{ padding: '20px' }}>
+      <div>
+        <p className='font-semibold text-2xl'>Gemini</p></div>
+
+      {/* <div className='flex gap-10'>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+        <p>Home</p>
+      </div> */}
+
+      <img src={assets.user_icon} className='w-15 h-15 rounded-full' alt="" />
+    </div>
+  )
+}
+
+export default Navbar
