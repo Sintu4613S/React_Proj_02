157deae148a00d2f6f9d1ae116f3fba9dd3dc2cc
Sintu Kumar <shiva49767@gmail.com>
2026-08-13 10:52:34 +0530

    process of importin and add and delete images

---
+++ gemini-proj/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { assets } from '../../assets/assest'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+        <img src={assets.menu_icon} alt="" />
+
+      </div>
+      <div className="bottom">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
