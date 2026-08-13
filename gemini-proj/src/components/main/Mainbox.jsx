f089c59df1f3a44ecf74c07ed434a12363ee27a8
Sintu Kumar <shiva49767@gmail.com>
2026-08-13 14:10:39 +0530

    In mainbox simple create a greet message with the help of css

---
+++ gemini-proj/src/components/main/Mainbox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Mainbox.css'
+
+const Mainbox = () => {
+  return (
+    <div className='main-container'>
+      <div className='greet'>
+        <p><span>Hello, Dev.</span></p>
+        <p>How can I help you today ?</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Mainbox
