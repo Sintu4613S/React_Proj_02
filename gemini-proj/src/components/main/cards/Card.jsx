cbc2d6ce49a24487a23f23d2addc47e8cdb93519
Sintu Kumar <shiva49767@gmail.com>
2026-08-14 21:04:43 +0530

    add the padding to card

--- gemini-proj/src/components/main/cards/Card.jsx
+++ gemini-proj/src/components/main/cards/Card.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../../assests/assest'
 
 const Card = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5'>
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ' style={{ padding: '20px' }}>
 
       <Singlecard title='heloo' imageSrc={assets.bulb_icon} />
       <Singlecard title='heloo' imageSrc={assets.code_icon} />
