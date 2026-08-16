79ae7300a0456eed3bcdced2197e861dd4124cfc
Sintu Kumar <shiva49767@gmail.com>
2026-08-16 20:38:13 +0530

    add the gemini icon and three hr during the loading and design them using tailwindcss and default css

--- gemini-proj/src/components/main/ResponseDisplay.jsx
+++ gemini-proj/src/components/main/ResponseDisplay.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './Response.css'
 import ReactMarkdown from 'react-markdown'
+import { assets } from '../../assests/assest'
 
 const ResponseDisplay = ({ loading, result }) => {
   if (loading) {
     return (
-      <div className='response-loading'>
+      <div className='response-loading flex flex-col '>
         <p className='text-2xl'>Thinking...</p>
+        <img src={assets.gemini_icon} className='w-15 p-5' alt="" />
+        <div className='loader'>
+          <hr />
+          <hr />
+          <hr />
+        </div>
       </div>
     )
   }
