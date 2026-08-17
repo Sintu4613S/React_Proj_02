b39bd94a8f6a07000a878e79f23e3c29ba251d2b
Sintu Kumar <shiva49767@gmail.com>
2026-08-17 11:08:49 +0530

    Enhance Sidebar component with newChat functionality and active index handling

--- gemini-proj/src/components/sidebar/Sidebar.jsx
+++ gemini-proj/src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react'
 import { assets } from '../../assests/assest'
 import './Sidebar.css'
 import { useMyContext } from '../../context/MyContext'
+import Mainbox from '../main/Mainbox'
 
 
 
@@ -9,15 +10,18 @@ const Sidebar = () => {
 
   const [sidebaropen, setSideBarOpen] = useState(false)
 
-  const { recentSearch } = useMyContext()
+  const { recentSearch, newChat, activeIndex, setActiveIndex } = useMyContext()
   console.log(recentSearch)
+  console.log('Active index kya h:', activeIndex)
 
   return (
     <div className='sidebar'>
       <div className="top">
         <img onClick={() => setSideBarOpen(!sidebaropen)} className='menu' src={assets.menu_icon} alt="" />
 
-        <div className='new-chat'>
+        <div
+          onClick={newChat}
+          className='new-chat'>
           <img src={assets.plus_icon} alt="" />
           {sidebaropen && <p style={{ animation: 'fadeIn 2s  forwards' }}>New Chat</p>}
         </div>
@@ -26,9 +30,14 @@ const Sidebar = () => {
           <div className="recent">
             <p className='recent-title'>Recent</p>
             {recentSearch && recentSearch.map((item, index) => (
-              <div key={index} className="recent-entry">
+              <div style={{ marginBottom: '10px' }} onClick={() => setActiveIndex(index)}
+                className={`recent-entry   ${activeIndex === index
+                  ? 'bg-blue-50 text-blue-600 font-medium'
+                  : 'text-gray-600 hover:bg-gray-100'
+                  }`}
+                key={index}>
                 <img src={assets.message_icon} alt="" />
-                <p style={{ animation: 'fadeIn 2s  forwards' }}>{item}</p>
+                <p style={{ animation: 'fadeIn 2s  forwards' }}>{item.slice(0, 10)}...</p>
                 {/* <button className='close'>X</button> */}
               </div>
             ))}
