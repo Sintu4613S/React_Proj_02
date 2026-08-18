6e23db5edf6cd351d7c8baa66efee8183ba65a49
Sintu Kumar <shiva49767@gmail.com>
2026-08-18 12:29:20 +0530

    feat: add About and Program components to App for improved navigation

--- edu-sphere/src/App.jsx
+++ edu-sphere/src/App.jsx
@@ -2,6 +2,8 @@
 import './App.css'
 import Hero from './components/hero/Hero'
 import Navbar from './components/navbar/Navbar'
+import About from './components/pages/about/About'
+import Program from './components/pages/programs/Program'
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Program />
+      <About />
     </>
   )
 }
