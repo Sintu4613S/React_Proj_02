f9347b1c90542e520397835503c615b9a0d789ea
Sintu Kumar <shiva49767@gmail.com>
2026-08-18 13:02:46 +0530

    Create a About.jsx and its css file

---
+++ edu-sphere/src/components/pages/about/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { assets } from '../../../assets/assets'
+import './About.css'
+const About = () => {
+  return (
+    <div className='about'>
+      <div className="left">
+        <img src={assets.about} alt="" />
+      </div>
+      <div className="right">
+        <h3>About University</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>Embark on a transformative educational journey with our university's comprehensive education programs. Our cutting-edge curriculum is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+        <p>With a focus on innovation, hands-on learning, and personalized mentorship, our programs prepare aspiring educators to make a meaningful impact in classrooms, schools, and communities.</p>
+        <p>Whether you aspire to become a teacher, administrator, counselor, or educational leader, our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
