590888ea6d1bd701e1bccbabb69399b27e3b6f54
Sintu Kumar <shiva49767@gmail.com>
2026-08-19 20:27:05 +0530

    Refactor Navbar component: replace anchor tags with Link components for routing

--- edu-sphere/src/components/navbar/Navbar.jsx
+++ edu-sphere/src/components/navbar/Navbar.jsx
@@ -1,38 +1,33 @@
 import { useEffect, useState } from 'react'
 import { assets } from '../../assets/assets'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   const [sidebar, setSidebarOpen] = useState(false)
-  // useEffect(() => {
-  //   setSiderbarOpen(true)
-  // } [])
   return (
     <nav className='containers-fluid  dark-nav flex items-center justify-between' >
       <img src={assets.logo} className='w-40' alt="" />
       <div className=' hidden md:flex items-center items-list '>
-        <a href="">Home</a>
-        <a href="">Program</a>
-        <a href="">AboutUs</a>
-        <a href="">Campus</a>
-        <a href="">Testimonials</a>
+        <Link to='/'>Home  </Link>
+        <Link to='/program'>Program  </Link>
+        <Link to='/about-us'>AboutUs  </Link>
+        <Link to='/campus'>Campus  </Link>
+        <Link to='/testimonial'>Testimonials  </Link>
         <button className='btn'>
-          <a href=''>ContactUs</a>
+          <Link to='/contact-us'>ContactUs  </Link>
         </button>
-        {/* <button className='sm:hidden' onClick={() => setSiderbarOpen(false)} className='btn'>
-          Close
-        </button> */}
       </div>
 
       {/* <nav className='containers-fluid  dark-nav' >
         <img src={assets.logo} className='w-40' alt="" /> */}
       {sidebar && (<div className='mobile-sidebar flex flex-col items-center gap-6 '>
-        <a href="">Home</a>
-        <a href="">Program</a>
-        <a href="">AboutUs</a>
-        <a href="">Campus</a>
-        <a href="">Testimonials</a>
+        <Link to='/'>Home  </Link>
+        <Link to='/program'>Program  </Link>
+        <Link to='/about-us'>AboutUs  </Link>
+        <Link to='/campus'>Campus  </Link>
+        <Link to='/testimonial'>Testimonials  </Link>
         <button className='btn'>
-          <a href=''>ContactUs</a>
+          <Link to='/contact-us'>ContactUs  </Link>
         </button>
         <button className='sm:hidden' onClick={() => setSidebarOpen(false)} className='btn md:hidden'>
           Close
