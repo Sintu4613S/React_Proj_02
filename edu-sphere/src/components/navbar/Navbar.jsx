47dcb91174e652c6257e09b345455cb60d0858c1
Sintu Kumar <shiva49767@gmail.com>
2026-08-19 15:06:58 +0530

    Refactor Navbar component: enhance responsiveness and update styles

--- edu-sphere/src/components/navbar/Navbar.jsx
+++ edu-sphere/src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from 'react'
 import { assets } from '../../assets/assets'
 import './Navbar.css'
-
 const Navbar = () => {
+  const [sidebar, setSidebarOpen] = useState(false)
+  // useEffect(() => {
+  //   setSiderbarOpen(true)
+  // } [])
   return (
-    <nav className='containers-fluid ' >
+    <nav className='containers-fluid  dark-nav flex items-center justify-between' >
       <img src={assets.logo} className='w-40' alt="" />
-      <div className='items-list'>
+      <div className=' hidden md:flex items-center items-list '>
         <a href="">Home</a>
         <a href="">Program</a>
         <a href="">AboutUs</a>
@@ -14,7 +18,30 @@ const Navbar = () => {
         <button className='btn'>
           <a href=''>ContactUs</a>
         </button>
+        {/* <button className='sm:hidden' onClick={() => setSiderbarOpen(false)} className='btn'>
+          Close
+        </button> */}
       </div>
+
+      {/* <nav className='containers-fluid  dark-nav' >
+        <img src={assets.logo} className='w-40' alt="" /> */}
+      {sidebar && (<div className='mobile-sidebar flex flex-col items-center gap-6 '>
+        <a href="">Home</a>
+        <a href="">Program</a>
+        <a href="">AboutUs</a>
+        <a href="">Campus</a>
+        <a href="">Testimonials</a>
+        <button className='btn'>
+          <a href=''>ContactUs</a>
+        </button>
+        <button className='sm:hidden' onClick={() => setSidebarOpen(false)} className='btn md:hidden'>
+          Close
+        </button>
+      </div>
+      )}
+      <button onClick={() => setSidebarOpen(!sidebar)} className='menu-icon md:hidden block'>
+        <img src={assets.menuIcon} className='w-8' alt="" />
+      </button>
 
     </nav>
   )
