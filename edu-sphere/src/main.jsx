9e44185750e6394fdb13a084ab6688cc6e6c4556
Sintu Kumar <shiva49767@gmail.com>
2026-08-19 20:27:47 +0530

    Implement routing with RouterProvider and update main component structure

--- edu-sphere/src/main.jsx
+++ edu-sphere/src/main.jsx
@@ -1,9 +1,49 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Hero from './components/hero/Hero.jsx'
+import Program from './components/pages/programs/Program.jsx'
+import ContactUs from './components/pages/contactUs/ContactUs.jsx'
+import Testimonial from './components/pages/testimonials/Testimonial.jsx'
+import Campus from './components/pages/campus/Campus.jsx'
+import About from './components/pages/about/About.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Hero />
+      },
+      {
+        path: '/program',
+        element: <Program />
+      },
+      {
+        path: '/about-us',
+        element: <About />
+      },
+      {
+        path: '/campus',
+        element: <Campus />
+      },
+      {
+        path: '/testimonial',
+        element: <Testimonial />
+      },
+      {
+        path: '/contact-us',
+        element: <ContactUs />
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
 
-  <App />
+  <RouterProvider router={router} />
 
 )
